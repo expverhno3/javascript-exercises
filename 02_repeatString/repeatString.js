580db9eb3a1d0657ec0507dbe9a15cfaf89371db
expverhno3 <flyed666@outlook.com>
2024-05-23 20:50:21 +0800

    Finish first two tasks

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let result = ''
+    if (num < 0) {
+        return "ERROR"
+    }
 
+    for (let i=0; i < num; i += 1){
+        result += string
+    } 
+    return result
 };
 
 // Do not edit below this line
